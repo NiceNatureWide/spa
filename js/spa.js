4fff80cf65c0e99cfed7649e022e4de3a3f8617f
NiceNatureWide <nice.nature.wide@gmail.com>
2019-04-20 00:33:39 +0900

    first commit

---
+++ js/spa.js
@@ -0,0 +1,19 @@
+/*js lint   browser: true, continue: true,
+devel: true, indent: 2, maxerr: 50,
+newcap: true, nomen: true, plusplus: true,
+regexp: true, sloppy: true, vars: false
+white: true
+*/
+/* global $, spa: true */
+
+let spa = (function() {
+  let initModule = function($container) {
+    $container.html(
+      '<h1 style="display:inline-block; margin:25px;">' +
+        "hello world!" +
+        "</h1>"
+    );
+  };
+
+  return { initModule: initModule };
+})();
